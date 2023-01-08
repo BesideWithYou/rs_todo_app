340fe3a43530b0187b3cdbc8d8fb5c7a12f31d0f
JiquanWang99 <951873902@qq.com>
2023-01-09 00:51:00 +0800

    chore: update comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+// 有一点点像 import process from './process.rs'
 mod process;
 mod state;
 mod todo;
 
+// 使用外部的库、标准库或自己定义的工具
 use process::process_input;
 use serde_json::Map;
 use serde_json::Value;
@@ -10,18 +12,25 @@ use std::env;
 use todo::todo_factory;
 
 fn main() {
+    // 收集所有命令行的参数，转换成数组
     let args: Vec<String> = env::args().collect();
+    // 拿到第一个参数如 get、delete、edit、create
     let command: &String = &args[1];
+    // 第二个参数是用来记录事件的
     let title: &String = &args[2];
 
+    // 读取根目录的 state.json 文件并转换成 map json 结构
     let state: Map<String, Value> = read_file("./state.json");
 
+    // 如果事件已经存取过了，那就直接获取，没有就将其状态设置为 pending
     let status: String;
     match &state.get(title) {
         Some(result) => status = result.to_string().replace('\"', ""),
         None => status = "pending".to_string(),
     }
 
+    // todo_factory 工厂函数用于处理 pending 和 done 状态的输入
     let item = todo_factory(&status, title).expect(&status);
+    // 将状态输入到本地文件中
     process_input(item, command.to_string(), &state);
 }
--- src/todo/structs/base.rs
+++ src/todo/structs/base.rs
@@ -3,6 +3,7 @@ pub struct Base {
     pub status: String,
 }
 
+// 为这个 Base 数据结构实现一个 new 方法，并返回一个实例化后的数据结构
 impl Base {
     pub fn new(input_title: &str, input_status: &str) -> Base {
         Base {
